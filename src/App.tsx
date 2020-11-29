2e05bf5aeea2453f0e7f95ac827ec3bbb1fa47f0
Thor Brink <thor.brink@consid.se>
2020-11-29 19:50:28 +0100

    Add React Router (#2)
    * Add React Router
    
    * Update test
    
    Co-authored-by: Thor Brink <thorbrink@MBPsomtlhorThor.lan>

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <Link className="App-link" to="/">Home</Link>
+          <Link className="App-link" to="/testroute">Testroute</Link>
+        </header>
+      </div>
+      <Switch>
+          <Route path="/">
+            Home
+          </Route>
+          <Route path="/testroute">
+            Testroute
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
